dedcc79c6d237d1d9e5f393f9df11af33e3aa288
Iryna Lazareva <il337733@gmail.com>
2022-08-10 21:54:45 +0300

    add first part

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
 
+const cardCreated = createGalleryCard(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", cardCreated);
+
+function createGalleryCard(cards) {
+    return cards.map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="
+      ${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    }).join('');
+}
 console.log(galleryItems);
+
+galleryEl.addEventListener('click', onCardClick)
+function onCardClick(event) {
+    console.log(event)
+    if(!event.target.contains('gallery'))  {
+    return;
+    }
+    console.log(event.target.dataset.source);
+}
