ba65c940a7b03f90f60a379faf61956e37a59a4d
Iryna Lazareva <il337733@gmail.com>
2022-08-12 16:30:14 +0300

    add task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const galleryEl = document.querySelector(".gallery");
 
@@ -6,7 +6,8 @@ const cardCreated = createGalleryCard(galleryItems);
 galleryEl.insertAdjacentHTML("beforeend", cardCreated);
 
 function createGalleryCard(cards) {
-    return cards.map(({ preview, original, description }) => {
+  return cards
+    .map(({ preview, original, description }) => {
       return `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
@@ -18,17 +19,28 @@ function createGalleryCard(cards) {
     />
   </a>
 </div>`;
-    }).join('');
+    })
+    .join("");
 }
 console.log(galleryItems);
 
-galleryEl.addEventListener('click', onCardClick)
+galleryEl.addEventListener("click", onCardClick);
 
 function onCardClick(event) {
   event.preventDefault();
-    // console.log(event)
-    if (!event.target.classList.contains("gallery__image")) {
-      return;
-    }
-    console.log(event.target.dataset.source);
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const originalImg = event.target.dataset.source;
+
+  //add from basicLightbox
+  const modalWindow = basicLightbox.create(`
+    <img src="${originalImg}" width="800" height="600">
+`);
+  modalWindow.show();
+
+  document.body.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") modalWindow.close();
+  });
 }
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+
+const cardCreated = createGalleryCard(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", cardCreated);
+console.log(cardCreated)
+function createGalleryCard(cards) {
+    return cards
+    .map(({ preview, original, description }) => {
+    return `<li class="gallery__item"> <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+    }).join("");
+    
+}
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+captionDelay: 250,
+});
+console.log(gallery)
 
 console.log(galleryItems);
